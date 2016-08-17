75014a605d5a2af345ca7c898ab7dd01d44f8da2
Joseph <joseph.jr@gmail.com>
2016-08-17 17:29:29 +0100

    modified release bike to generate error message when there is no bike. not done with error message

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,13 +4,27 @@ describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
   it 'releases working bikes' do
-    bike = subject.release_bike
-    expect(bike).to be_working
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.release_bike).to be_working
+  end
+
+  describe '#release_bike' do
+    it 'releases a docked bike' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.release_bike).to eq bike
+
+    end
   end
 
   it { is_expected.to respond_to(:dock).with(1).argument }
 
   it { is_expected.to respond_to(:bike) }
+
+  #it 'raises an error' do
+  #expect { NoMethodError }.to raise_error(NoMethodError)
+  #end
 
   it 'docks something' do
     bike = Bike.new
