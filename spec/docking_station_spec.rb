f1a4bbf17d50efcecc8de84dc0c9a48ac9074ff2
Lawrence Dawson <lawrencedawson@Lawrences-MacBook-Pro.local>
2016-08-17 18:28:14 +0100

    Completed chp 12, Error when no bikes available now works, all tests  pass

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -14,7 +14,11 @@ describe DockingStation do
     bike = Bike.new
     subject.dock(bike)
     expect(subject.release_bike).to eq bike
+  end
 
+
+    it 'raises an error when there are no bikes available' do
+      expect { subject.release_bike }.to raise_error 'No bikes available'
     end
   end
 
