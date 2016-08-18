414e7af5ac320be56058961169178c73c893bc96
Joseph <joseph.jr@gmail.com>
2016-08-18 12:20:30 +0100

    step 15

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -7,15 +7,22 @@ class DockingStation
   end
 
   def release_bike
-    fail 'No bikes available' if @bike_store.empty?
+    fail 'No bikes available' if vacuous?
     @bike_store.pop
   end
 
   def dock(bike)
-    fail 'Docking Station is full' if @bike_store.length >= 20
+    fail 'Docking Station is full' if full?
     @bike_store << bike
   end
 
 private
   attr_reader :bike_store
+
+  def full?
+    @bike_store.length >= 20
+  end
+  def vacuous?
+    @bike_store == []
+  end
 end
