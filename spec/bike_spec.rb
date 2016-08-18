d5b9a982b49cb2909e1a5f5765a1c344479af461
TomStuart92 <Thomascstuart@gmail.com>
2016-08-18 13:01:13 +0100

    WIP - Chapter 18

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,5 +1,14 @@
 require 'Bike'
 
 describe Bike do
-  it { is_expected.to respond_to :working?}
+ it 'should be working when created' do
+   expect(subject.working?).to eq(true)
+ end
+
+it 'should be able to be reported broken' do
+  bike = Bike.new
+  bike.report_broken
+  expect(bike.working?).to eq(false)
+end
+
 end
