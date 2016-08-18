7a957fff8a0fadf3fa98827eadbc32d2b9873052
TomStuart92 <Thomascstuart@gmail.com>
2016-08-18 15:06:51 +0100

    Step 20

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,15 +3,14 @@ require 'docking_station'
 describe DockingStation do
 
 before do
-  @bike = double(:bike)
-  allow(@bike).to receive(:working?).and_return(true)
-  allow(@bike).to receive(:report_broken).and_return(false)
+  @working_bike = double(:bike, :working? => true)
+  @broken_bike = double(:bike, :working? => false)
 end
 
   describe '#release_bike' do
     it 'releases a docked bike' do
-      subject.dock(@bike)
-      expect(subject.release_bike).to eq(@bike)
+      subject.dock(@working_bike)
+      expect(subject.release_bike).to eq(@working_bike)
     end
 
     it 'raises an error when there are no bikes available' do
@@ -19,32 +18,28 @@ end
     end
 
     it 'should not release broken bikes' do
-      @bike.report_broken
-      allow(@bike).to receive(:working?).and_return(false)
-      subject.dock(@bike)
+      subject.dock(@broken_bike)
       expect{subject.release_bike}.to raise_error 'No bikes available'
     end
   end
 
   describe 'docking bikes' do
     it 'docks something' do
-      expect(subject.dock(@bike)).to eq [@bike]
+      expect(subject.dock(@working_bike)).to eq [@working_bike]
     end
 
     it 'refuses more bikes when no capacity' do
-      DockingStation::DEFAULT_CAPACITY.times { subject.dock @bike }
-      expect {subject.dock(@bike)}.to raise_error 'Docking Station is full'
+      DockingStation::DEFAULT_CAPACITY.times { subject.dock @working_bike }
+      expect {subject.dock(@working_bike)}.to raise_error 'Docking Station is full'
     end
 
     it 'should accept broken bikes' do
-      @bike.report_broken
-      allow(@bike).to receive(:working?).and_return(false)
-      expect(subject.dock(@bike)).to eq([@bike])
+      expect(subject.dock(@broken_bike)).to eq([@broken_bike])
     end
     it 'allows up to 30 bikes' do
       docking_station = DockingStation.new(30)
-      30.times { docking_station.dock @bike }
-      expect {docking_station.dock(@bike)}.to raise_error 'Docking Station is full'
+      30.times { docking_station.dock @working_bike }
+      expect {docking_station.dock(@working_bike)}.to raise_error 'Docking Station is full'
     end
   end
 
